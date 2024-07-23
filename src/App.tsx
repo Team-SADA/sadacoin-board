527948fc4e05bd252831d6f6da8433d471617dd1
piz2a <43025513+piz2a@users.noreply.github.com>
2024-07-23 12:18:59 +0900

    Search & Scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import axios from 'axios';
 import './App.scss';
 
@@ -18,6 +18,13 @@ const reward: string[] = [
 
 function App() {
     const [board, setBoard] = useState<UserData[]>([]);
+    const [searchText, setSearchText] = useState<string>("");
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (inputRef.current !== null)
+            inputRef.current.focus();
+    });
 
     useEffect(() => {
         axios.get('https://sada.ziho.kr/coin/show').then((result) => {
@@ -32,14 +39,17 @@ function App() {
             <h1>
                 SADA Coin
                 <img src={"./coin.png"} alt={"coin"}/>
-                Top 10
             </h1>
             <p>
                 부스 종료 시 상위 5명에게는 경품이 주어집니다!<br/>
                 또한, 코인을 내고 작은 선물을 받아갈 수 있습니다.
             </p>
+            <input type={"text"} ref={inputRef} placeholder={"내 코인 수와 등수는? 이름 검색"} onChange={
+                (e) => setSearchText(e.target.value)
+            }/>
             <table>
-                <tbody>
+                <tbody className={"background"}></tbody>
+                <tbody className={"header"}>
                 <tr>
                     <th>순위</th>
                     <th>학번</th>
@@ -47,15 +57,19 @@ function App() {
                     <th>코인</th>
                     {/*<th className={"reward"}>상품</th>*/}
                 </tr>
-                {board.map((line, index) => (
-                    <tr>
-                        <td>{index + 1}</td>
-                        <td>{line.id}</td>
-                        <td>{line.name}</td>
-                        <td>{line.coin}<img src={"./coin.png"} alt={"coin"}/></td>
-                        {/*<td className={"reward"}>{index < 5 ? reward[index] : ''}</td>*/}
-                    </tr>
-                ))}
+                </tbody>
+                <tbody className={"board"}>
+                    {board.map((line, index) => (
+                        line.name.includes(searchText) ? (
+                                <tr>
+                                    <td>{index + 1}</td>
+                                    <td>{line.id}</td>
+                                    <td>{line.name}</td>
+                                    <td>{line.coin}<img src={"./coin.png"} alt={"coin"}/></td>
+                                    {/*<td className={"reward"}>{index < 5 ? reward[index] : ''}</td>*/}
+                                </tr>
+                            ) : <></>
+                    ))}
                 </tbody>
             </table>
         </div>
